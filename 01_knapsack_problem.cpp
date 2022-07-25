47e9f6677ece49c11c8c25bf3f4327f9f2e50f3c
rahulkushwaha-in <awesomerahul.vl@gmail.com>
2022-07-26 00:24:15 +0530

    tabulated method

--- 01_knapsack_problem.cpp
+++ 01_knapsack_problem.cpp
@@ -34,3 +34,46 @@
 // Space Complexity: O(N*W) + O(N)
 
 // Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*W)).
+
+
+
+
+
+
+
+
+
+
+// Tabulation method
+ int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       vector<vector<int>>dp(n,vector<int>(W+1,0));
+       for(int i =wt[0];i<=W;i++){
+           dp[0][i] = val[0];
+       }
+       
+       for(int ind = 1;ind<n;ind++){
+           for(int cap = 0;cap<=W;cap++){
+               int notTake = 0+ dp[ind-1][cap];
+               int take = INT_MIN;
+               if(wt[ind] <= cap){
+                   take = val[ind] + dp[ind-1][cap-wt[ind]];
+               }
+               dp[ind][cap] = max(notTake,take);
+           }
+       }
+       
+       return dp[n-1][W];
+       
+    
+    }
+
+
+    // Time Complexity: O(N*W)
+
+Reason: There are two nested loops
+
+// Space Complexity: O(N*W)
+
+// Reason: We are using an external array of size ‘N*W’. Stack Space is eliminated.
